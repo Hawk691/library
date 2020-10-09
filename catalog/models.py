7613391ea0cbd6fdce98ec5fa87269f56dfdfafd
marko <hawk691@gmail.com>
2020-10-09 10:41:17 +0300

    database correction

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 class Genre(models.Model):
     """
@@ -25,6 +26,7 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Characters code of the book')
     genre = models.ManyToManyField(Genre, help_text='Select a gere for this book')
+    language = models.ForeignKey('Language', verbose_name='Original language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         """
@@ -33,10 +35,68 @@ class Book(models.Model):
 
         return self.title
 
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular book instance
+        """
+
+        return reverse('book-detail', args=[str(self.id)])
+
 
 class Author(models.Model):
     """
-    Model representing an authors of books
+    Model representing an author of book
     """
-    pass
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField('Born', null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_abslute_url(self):
+        """
+        Returns the url to access a particular author instance
+        """
+
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+
+        return '%s, %s' % (self.last_name, self.first_name)
+
+    
+class BookInstance(models.Model):
+    """
+    Model representing a specific copy of the book, that can be borrowed from the library
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for book')
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+            ('m', 'Maintenance'),
+            ('o', 'On loan'),
+            ('a', 'Available'),
+            ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='a')
+
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """
+        String for representing the Model object
+        """
+        return '%s (%s)' % (self.id, self.book.title)
+
+class Language(models.Model):
+    lang = models.CharField(max_length=50)
 
