d0fd40bb8a60e6d074642b5085f5de187ec49ccb
marko <hawk691@gmail.com>
2020-10-20 15:44:24 +0300

    session param added

--- catalog/views.py
+++ catalog/views.py
@@ -14,11 +14,16 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
     num_genres = Genre.objects.count()
+
+    # init num_visits in session
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     return render(request, 'index.html', context={'num_books':num_books,
                                                   'num_instances':num_instances,
                                                   'num_instances_available':num_instances_available,
                                                   'num_authors':num_authors,
                                                   'num_genres':num_genres,
+                                                  'num_visits':num_visits,
                                                  }
                                             )
 
